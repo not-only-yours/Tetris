48afdb61ba037c917bb039e8de55c6935ac4b7b1
Nikita Bakun <not-only-yours@users.noreply.github.com>
2020-03-17 12:12:34 +0200

    buttons

--- main.py
+++ main.py
@@ -4,36 +4,20 @@ pygame.init()
 
 display_width = 400
 display_height = 600
-
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 
-block_color = (53, 115, 255)
-
-car_width = 73
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 clock = pygame.time.Clock()
-
-
-
-
-def things(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-
-
-
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
-
-
 def game_intro():
     intro = True
-
     while intro:
         for event in pygame.event.get():
             print(event)
@@ -44,11 +28,29 @@ def game_intro():
         gameDisplay.fill(white)
         largeText = pygame.font.Font('freesansbold.ttf', 115)
         TextSurf, TextRect = text_objects("Tetris", largeText)
+        button("GO!", 150, 350, 100, 50, red, black, )
+        button("Settings", 150, 450, 100, 50, red, black, )
         TextRect.center = ((display_width / 2), (display_height / 4))
         gameDisplay.blit(TextSurf, TextRect)
         pygame.display.update()
         clock.tick(15)
 
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
 
 game_intro()
 pygame.quit()
